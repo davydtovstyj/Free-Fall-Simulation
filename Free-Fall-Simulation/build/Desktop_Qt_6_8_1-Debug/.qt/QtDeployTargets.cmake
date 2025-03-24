717290e583ccd42191018cfcb7f5efc0b059655b
davydtovstyj <devidtolsto1@gmail.com>
2025-03-24 22:43:50 +0200

    Creating QT Quick Application

---
+++ Free-Fall-Simulation/build/Desktop_Qt_6_8_1-Debug/.qt/QtDeployTargets.cmake
@@ -0,0 +1,2 @@
+set(__QT_DEPLOY_TARGET_appFree-Fall-Simulation_FILE /home/udaff/Qt/Projects/Free-Fall-Simulation/Free-Fall-Simulation/build/Desktop_Qt_6_8_1-Debug/appFree-Fall-Simulation)
+set(__QT_DEPLOY_TARGET_appFree-Fall-Simulation_TYPE EXECUTABLE)
