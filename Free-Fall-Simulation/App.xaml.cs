adcbe014f559127e0ba9deaba74cc32afa9f9c4a
davydtovstyj <devidtolsto1@gmail.com>
2025-05-02 11:33:37 +0300

    Creating WPF project

---
+++ Free-Fall-Simulation/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Free_Fall_Simulation
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
